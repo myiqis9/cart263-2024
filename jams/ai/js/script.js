cf1c99d038b651f065f2fdbcbb929f89c4b6b5d6
Ema Radu <emv.rad@gmail.com>
2024-02-13 14:55:00 -0500

    starting AI jam
    e

---
+++ jams/ai/js/script.js
@@ -0,0 +1,29 @@
+/**
+ML.JS AI jam
+Viviana Ema Radu
+
+refs: https://learn.ml5js.org/#/reference/facemesh
+https://github.com/tensorflow/tfjs-models/tree/master/face-landmarks-detection#keypoints 
+
+*/
+
+"use strict";
+//reference image is in images folder
+
+
+//p5js preload
+function preload() {
+
+}
+
+
+//p5js setup
+function setup() {
+
+}
+
+
+//p5js draw
+function draw() {
+
+}
