809807d6f1210573408c7a9af4a3a53f909c527c
Ema Radu <83728442+myiqis9@users.noreply.github.com>
2024-02-25 22:50:35 -0500

    yayyy Im done!
    finished writing up the checks for emotions, the timeline dialogue and added a start loading screen.
    
    I changed how emotions are calculated to measure from a midpoint between the mouth corners and compare that to the actual upper lip middle point, since I noticed it gave wonky results otherwise if your head was just slightly inclined on one side or the other. all checks work really well for me and I also had lots of fun trying to cheat through them all
    
    I also finished commentating everything and hopefully it makes sense yaaaaaaaaaaaaaaay

--- jams/ai/js/keypoint.js
+++ jams/ai/js/keypoint.js
@@ -1,35 +1,66 @@
 class Keypoint {
-    constructor(name, x, y) {
-        this.name = name;
+    constructor(x, y) {
         this.x = x;
         this.y = y;
     }
 
-    //all functions are checked from mouth top point being 'this'
+    //all functions are checked from mouth middle top point being 'this'
     checkHappy(mLeft, mRight) {
         print('checking happy');
-        if(mLeft.y < this.y && mRight.y < this.y) {
-            print('happy!!!');
-            return true;
-        }
-        else {
-            print('not happy :(');
-            return false;
-        }
+
+        //calculate midpoint between two mouth corners on Y axis
+        let midY = (mLeft.y + mRight.y)/2;
+        print(`midpoint: ${midY} upper mouth: ${this.y}`);
+
+        //if midpoint is same or higher than upper mouth part then youre smiling!
+        if(midY <= this.y) return true;
+        else return false;
     }
 
     checkFrown(mLeft, mRight) {
+        print('checking frown');
 
+        //calculate midpoint between two mouth corners on Y axis
+        let midY = (mLeft.y + mRight.y)/2;
+        print(`midpoint: ${midY} upper mouth: ${this.y}`);
+
+        //if midpoint is lower than upper mouth part then youre frowning!
+        //this one can be cheated in many ways, such as just opening your mouth. but that's kind of the point. :)
+        if(midY > this.y) return true;
+        else return false;
     }
 
     checkSurprised(mBottom) {
-        print('checking mouth open');
+        print('checking surprised'); //mouth open
+
+        //check distance between upper and lower lip
         let d = dist(this.x, this.y, mBottom.x, mBottom.y);
-        if(d > 3) print('mouth open!!!');
-        else print('not open :(');
+        print(`dist: ${d}`);
+
+        //if distance is higher than 5 (approx number) then mouth is open!
+        if(d > 5) return true;
+        else return false;
     }
 
-    checkOverjoyed(mBottom, mLeft, mRight) {
+    checkOverjoyed(mBottom, mLeft, mRight, cLeft, cRight) {
+        //ok bear with me
+        print('checking overjoyed'); //mouth open AND smiling
 
+        //it first checks if you're smiling
+        //since the mouth is open the middle part of the mouth isnt a reliable reference point
+        //so we compare the middle Y point between mouth corners with the middle Y point of both cheeks,
+        //which would normally be lower with an open mouth unless the person is smiling!
+        let midY = (mLeft.y + mRight.y)/2;
+        let cheekY = (cLeft.y + cRight.y)/2;
+        print(`mouth midpoint: ${midY} cheek midpoint: ${cheekY}`);
+
+        if(midY < cheekY) { //if smile Y is higher than cheek Y
+            //then, it checks if the mouth is also actually open
+            let d = dist(this.x, this.y, mBottom.x, mBottom.y);
+            print(`dist: ${d}`);
+
+            if(d > 5) return true;
+            else return false;
+        }
     }
 }
--- jams/ai/js/script.js
+++ jams/ai/js/script.js
@@ -9,46 +9,52 @@ reference image for keypoints is in images folder
 */
 "use strict";
 
+let state = 'loading'; //state of loading facemesh
+
 let facemesh;
 let video;
-let predictions = [];
+let predictions = []; //facemesh predictions array
 
-let timer;
-let flash;
-let blind = 0;
-let countdown = 0;
+let timer; //timer for camera
+let flash; //camera flash
+let blind = 0; //flash duration
+let countdown = 0; //countdown displayed
 
 //array of all keypoints
 let keypoints = [];
+//array of randomized keypoints for animation
+let randomized = [];
 
-let emotion = 'happy';
-let predicting = false;
-let success = false;
-let display = true;
+let emotion = 'happy'; //emotion it checks for
+let predicting = false; //is the emotion prediction happening?
+let success = false; //has user displayed correct emotion?
+let display = false; //is dialogue displaying/is user able to interact?
 
+//timeline of dialogue
 let timeline = [
-/* 0 */ 'Welcome! Click anywhere to start.',
-/* 1 */ 'Texttttt',
-/* 2 */ 'Text 3',
-/* 3 */ 'Text 4',
-/* 4 */ 'Check happens next',
+/* 0 */ "Welcome! Click anywhere to start.",
+/* 1 */ "This is a test designed to verify that you are human.",
+/* 2 */ "Please provide a facial expression that would match with the prompt of each test.",
+/* 3 */ "Make sure to face the camera head-on and to lean slightly downwards for the best results.",
+/* 4 */ "First prompt: smile for the camera! Are you ready?",
 /* flash here */
-/* 5 */ 'Go here if check successful',
-/* 6 */ 'Blah blah',
-/* 7 */ 'Another check next',
+/* 5 */ "Great! You have passed the first test.",
+/* 6 */ "And what a beautiful smile you have!",
+/* 7 */ "Second prompt: you've had a terrible day! How sad! Are you ready?",
 /* flash here */
-/* 8 */ 'Go here if check successful',
-/* 9 */ 'Blaaaaa',
-/* 10 */ 'Another check',
+/* 8 */ "Yay! You passed the second test!",
+/* 9 */ "You may wipe that frown from your face! Happier times are ahead.",
+/* 10 */ "Third prompt: you just heard some shocking news! Are you ready?",
 /* flash here */
-/* 11 */ 'Check success',
-/* 12 */ 'Blah blah',
-/* 13 */ 'Check next',
+/* 11 */ "Third test was a success!",
+/* 12 */ "The next one will be even more surprising! Prepare yourself.",
+/* 13 */ "Final prompt: you have just won a million dollar lottery! Are you ready?",
 /* flash here */
-/* 14 */ 'Check success',
-/* 15 */ 'You passed every check!!!',
-/* 16 */ 'This is the check failure',
-/* 17 */ 'Would you like to retry?'
+/* 14 */ "Congratulations! You successfully passed every check!!!",
+/* 15 */ "We have confirmed you are human. You may celebrate your epic human status, or click anywhere to take the test again!",
+/* failure */
+/* 16 */ "Sorry, our data results concluded you are not human! You may not proceed.",
+/* 17 */ "Would you like to retry? Click anywhere to restart."
 ];
 
 let ti = 0; //timeline index, to check what point of the timeline to go to depending on results
@@ -70,6 +76,8 @@ function setup() {
 
 function startSequence() {
     console.log("Model ready!");
+    state = 'predicting';
+    display = true;
 }
 
 //advances text timeline
@@ -94,25 +102,32 @@ function checkTimeline() {
 //bottom lip: 14
 //left corner mouth: 61
 //right corner mouth: 291
+//left cheek: 192
+//right cheek: 416
 function findKeypoints() {
     if(predictions.length > 0) {
         let face = predictions[0].scaledMesh;
 
-        let top = new Keypoint('top', face[13][0], face[13][1]);
-        let bottom = new Keypoint('bottom', face[14][0], face[14][1]);
-        let left = new Keypoint('left', face[61][0], face[61][1]);
-        let right = new Keypoint('right', face[291][0], face[291][1]);
+        let top = new Keypoint(face[13][0], face[13][1]);
+        let bottom = new Keypoint(face[14][0], face[14][1]);
+        let mleft = new Keypoint(face[57][0], face[57][1]);
+        let mright = new Keypoint(face[287][0], face[287][1]);
+        let cleft = new Keypoint(face[192][0], face[192][1]);
+        let cright = new Keypoint(face[416][0], face[416][1]);
 
-        keypoints = [top, bottom, left, right];
+        //make keypoints array update to these keypoints
+        keypoints = [top, bottom, mleft, mright, cleft, cright];
         checkEmotion();
     }
 }
 
 //keypoints array order: 
-// 0 top
-// 1 bottom
-// 2 left
-// 3 right
+// 0 mouth top
+// 1 mouth bottom
+// 2 mouth left
+// 3 mouth right
+// 4 cheek left
+// 5 cheek right
 //this happens during a small window frame of 0.1 second at the start of the camera flash
 //the last result obtained is retained in bool success
 function checkEmotion() {
@@ -127,7 +142,7 @@ function checkEmotion() {
             success = keypoints[0].checkSurprised(keypoints[1]) ? true : false;
             break;
         case 'overjoyed':
-            success = keypoints[0].checkOverjoyed(keypoints[1], keypoints[2], keypoints[3]) ? true : false;
+            success = keypoints[0].checkOverjoyed(keypoints[1], keypoints[2], keypoints[3], keypoints[4], keypoints[5]) ? true : false;
             break;
     }
 }
@@ -153,39 +168,82 @@ function checkResults() {
 
 //p5js draw
 function draw() {
+    if(state == 'loading') load();
+    else if (state == 'predicting') predict();
+}
+
+//p5js draw if facemesh is still loading
+function load() {
+    background(0);
+
+    push();
+    noStroke();
+    textAlign(CENTER, CENTER);
+    textSize(24);
+    fill(255);
+    text('Loading Facemesh...', width/2, height/2);
+    pop();
+}
+
+//p5js draw if facemesh loaded
+function predict() {
     image(video, 0, 0, width, height);
     if(blind > 0) drawBlind();
     if(countdown > 0) drawCountdown();
     if(display) drawTranscript();
+    else drawKeypoints();
     if(predicting) findKeypoints();
-    drawKeypoints();
 }
 
-//keypoints draw (temporary)
+//randomizer for drawKeypoints() below (called every flash frame)
+function randomize() {
+    if(predictions.length > 0) {
+        print('randomizing');
+        let face = predictions[0].scaledMesh;
+
+        //four random points on the facemesh. it doesn't really matter if they're duplicate, it adds to the randomness!
+        //convert them to an integer with int() because otherwise the p5js random goes crazy
+        let r1 = int(random(face.length));
+        let r2 = int(random(face.length));
+        let r3 = int(random(face.length));
+        let r4 = int(random(face.length));
+        let r5 = int(random(face.length));
+
+        //randomized array update to these new randoms
+        randomized = [face[r1], face[r2], face[r3], face[r4], face[r5]];
+    }
+}
+
+//funny little short animation drawn while the flash is happening (when display is false)
 function drawKeypoints() {
-    let index = 0;
-    for(let key of keypoints) {
+    for(let i = 0; i < randomized.length; i++) {
+        push();
         fill(255);
-        textSize(10);
-        textAlign(CENTER, CENTER);
-        text(index, key.x, key.y);
-        index++;
+        ellipseMode(CENTER);
+        stroke(0);
+        strokeWeight(1);
+        ellipse(randomized[i][0], randomized[i][1], 5, 5);
+        pop();
     }
 }
 
 //'camera' flash effect draw
 function drawBlind() {
-    push();
-    rectMode(CENTER);
-    noStroke();
-    fill(255, 255, 255, blind);
-    rect(width/2, height/2, width, height);
-    pop();
+    if(blind > 0) {
+        push();
+        rectMode(CENTER);
+        noStroke();
+        fill(255, 255, 255, blind);
+        rect(width/2, height/2, width, height);
+        pop();
+    }
 }
 
 //countdown draw
 function drawCountdown() {
     push();
+    stroke(0);
+    strokeWeight(3);
     textAlign(CENTER, CENTER);
     textSize(54);
     fill(255);
@@ -196,10 +254,14 @@ function drawCountdown() {
 //draw the dialogue transcript from the timeline
 function drawTranscript() {
     push();
+    stroke(0);
+    strokeWeight(3);
     textAlign(CENTER, CENTER);
+    textWrap(WORD);
     textSize(24);
     fill(255);
-    text(timeline[ti], width/2, height/2+100);
+    //p5js text wrap always offsets it in such a weird way, idk why it does that. 225 to make up for offset
+    text(timeline[ti], width/2-225, height/2+150, 450);
     pop();
 }
 
@@ -230,18 +292,20 @@ function startFlash() {
 //camera flash setInterval
 function flashInterval() {
     blind -= 55;
+    randomize(); //randomized animation
 
     //set very short interval mid-flash in which it predicts the face
     if(blind < 201) predicting = false;
 
-    if(blind <= 0) {
+    if(blind <= -250) { //goes a bit over 0 for the random animation
         blind = 0;
         clearInterval(flash);
+        randomized = []; //empty randomized so it doesnt print during the next countdown
         checkResults();
     }
 }
 
 //user advances dialogue in timeline with mouseclick
 function mousePressed() {
-    if(display) checkTimeline();
+    if(state == 'predicting' && display) checkTimeline();
 }
